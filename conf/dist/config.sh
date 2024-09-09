75ed454625a52ecdfb08649f6ca11b474f598586
Jellypowered <jellypowered@gmail.com>
2024-09-09 03:25:35 -0500

    Change to build DB_IMPORT tool

--- conf/dist/config.sh
+++ conf/dist/config.sh
@@ -84,7 +84,7 @@ CCOREPCH=${CCOREPCH:-ON}
 CAPPS_BUILD=${CAPPS_BUILD:-all}
 
 # build tools list variable
-CTOOLS_BUILD=${CTOOLS_BUILD:-none}
+CTOOLS_BUILD=${CTOOLS_BUILD:-all}
 
 # build apps list
 CBUILD_APPS_LIST=${CBUILD_APPS_LIST:-''}
